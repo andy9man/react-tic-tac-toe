19343af0bef89579ac3184e4fa9433c7a7060dd9
Andrew N <aninneman@gmail.com>
2018-01-19 14:27:19 -0600

    Added display functionality

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import './App.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square" onClick={ () => {this.props.squareHandler(this.props.id);}}>
-
+      <button className="square"  onClick={ () => {this.props.squareHandler(this.props.id);}}>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square id={i} squareHandler={this.props.squareHandler} />;
+  renderSquare(i, value, enabled) {
+    return <Square id={i} value={value} enabled={enabled} squareHandler={this.props.squareHandler} />;
   }
 
   render() {
@@ -23,19 +23,19 @@ class Board extends React.Component {
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this.renderSquare(0, this.props.board[0].value, this.props.board[0].enabled)}
+          {this.renderSquare(1, this.props.board[1].value, this.props.board[1].enabled)}
+          {this.renderSquare(2, this.props.board[2].value, this.props.board[2].enabled)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {this.renderSquare(3, this.props.board[3].value, this.props.board[3].enabled)}
+          {this.renderSquare(4, this.props.board[4].value, this.props.board[4].enabled)}
+          {this.renderSquare(5, this.props.board[5].value, this.props.board[5].enabled)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {this.renderSquare(6, this.props.board[6].value, this.props.board[6].enabled)}
+          {this.renderSquare(7, this.props.board[7].value, this.props.board[7].enabled)}
+          {this.renderSquare(8, this.props.board[8].value, this.props.board[8].enabled)}
         </div>
       </div>
     );
@@ -47,11 +47,11 @@ class Game extends React.Component {
     super(props);
     this.state = {
       player: [
-        {'id':'player1', 'name':'Player 1'},
-        {'id':'player2', 'name':'Player 2'}
+        {'id':'player1', 'name':'Player 1', 'sign':'X'},
+        {'id':'player2', 'name':'Player 2', 'sign':'O'}
       ],
 
-      turn: 'player1',
+      turn:true,
 
       board: [
         {'cellId':0, 'value':null, 'enabled':true},
@@ -66,17 +66,21 @@ class Game extends React.Component {
       ]
     }
     this.squareHandler = this.squareHandler.bind(this);
+    this.getSign = this.getSign.bind(this);
   }
-
+getSign(){
+  return this.state.turn ? 'X': 'O';
+}
   squareHandler( cellId ) {
     let arr = this.state.board.map( item => {
       if(cellId === item.cellId){
-        return {'cellId':item.cellId, 'value':'X', 'enabled':false};
+        return {'cellId':item.cellId, 'value': this.getSign(), 'enabled':false};
       }
       else{ return item; }
     })
-    this.setState({board: arr}, () => {
+    this.setState({board: arr, turn:!this.state.turn} , () => {
       console.log(this.state.board);
+
     });
 
   }
@@ -84,7 +88,7 @@ class Game extends React.Component {
     return (
       <div className="game">
         <div className="game-board">
-          <Board squareHandler={this.squareHandler} />
+          <Board squareHandler={this.squareHandler} board={this.state.board} />
         </div>
         <div className="game-info">
           <div>{/* status */}</div>
